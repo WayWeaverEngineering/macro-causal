89bc5d2564653427e60105b560f1163aa6d79582
harry-nguyen-wayweaver <harrynguyen@wayweaver.com>
2025-08-30 19:06:14 -0400

    Use preloaded lambda layers

--- backend/cdk/infrastructure/configs/LambdaConfig.ts
+++ backend/cdk/infrastructure/configs/LambdaConfig.ts
@@ -12,7 +12,10 @@ export class LambdaConfig {
     return `${LambdaConfig.LAMBDA_PYTHON_CODE_FOLDER}/${subPath}`;
   }
 
-  static getLambdaPythonLayerPath(subPath: string) {
-    return `${LambdaConfig.LAMBDA_PYTHON_CODE_FOLDER}/layers/${subPath}`;
-  }
+  static readonly BOTO_LAMBDA_LAYER_ARN = "arn:aws:lambda:us-east-1:715067592333:layer:macro-causal-boto-lambda-layer:12";
+  static readonly NUMPY_LAMBDA_LAYER_ARN = "arn:aws:lambda:us-east-1:715067592333:layer:macro-causal-numpy-lambda-layer:12";
+  static readonly PANDAS_LAMBDA_LAYER_ARN = "arn:aws:lambda:us-east-1:715067592333:layer:macro-causal-pandas-lambda-layer:12";
+  static readonly REQUESTS_LAMBDA_LAYER_ARN = "arn:aws:lambda:us-east-1:715067592333:layer:macro-causal-requests-lambda-layer:8";
+  static readonly YFINANCE_LAMBDA_LAYER_ARN = "arn:aws:lambda:us-east-1:715067592333:layer:macro-causal-yfinance-lambda-layer:12";
+  static readonly DATEUTILS_LAMBDA_LAYER_ARN = "arn:aws:lambda:us-east-1:715067592333:layer:macro-causal-dateutils-lambda-layer:8";
 }
--- backend/cdk/infrastructure/stacks/PythonLambdaLayersStack.ts
+++ backend/cdk/infrastructure/stacks/PythonLambdaLayersStack.ts
@@ -6,62 +6,32 @@ import { DefaultIdBuilder } from "../../utils/Naming";
 
 export class PythonLambdaLayersStack extends Stack {
 
-  public readonly botoLambdaLayer: lambda.LayerVersion;
-  public readonly numpyLambdaLayer: lambda.LayerVersion;
-  public readonly pandasLambdaLayer: lambda.LayerVersion;
-  public readonly yfinanceLambdaLayer: lambda.LayerVersion;
-  public readonly requestsLambdaLayer: lambda.LayerVersion;
-  public readonly dateutilsLambdaLayer: lambda.LayerVersion;
+  public readonly botoLambdaLayer: lambda.ILayerVersion;
+  public readonly numpyLambdaLayer: lambda.ILayerVersion;
+  public readonly pandasLambdaLayer: lambda.ILayerVersion;
+  public readonly yfinanceLambdaLayer: lambda.ILayerVersion;
+  public readonly requestsLambdaLayer: lambda.ILayerVersion;
+  public readonly dateutilsLambdaLayer: lambda.ILayerVersion;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const botoLambdaLayerId = DefaultIdBuilder.build('boto-lambda-layer');
-    this.botoLambdaLayer = new lambda.LayerVersion(this, botoLambdaLayerId, {
-      code: lambda.Code.fromAsset(LambdaConfig.getLambdaPythonLayerPath('boto')),
-      compatibleRuntimes: [LambdaConfig.DEFAULT_PYTHON_RUNTIME],
-      description: 'Python lambda layer for boto',
-      layerVersionName: botoLambdaLayerId
-    });
+    this.botoLambdaLayer = lambda.LayerVersion.fromLayerVersionArn(this, botoLambdaLayerId, LambdaConfig.BOTO_LAMBDA_LAYER_ARN);
 
     const requestsLambdaLayerId = DefaultIdBuilder.build('requests-lambda-layer');
-    this.requestsLambdaLayer = new lambda.LayerVersion(this, requestsLambdaLayerId, {
-      code: lambda.Code.fromAsset(LambdaConfig.getLambdaPythonLayerPath('requests')),
-      compatibleRuntimes: [LambdaConfig.DEFAULT_PYTHON_RUNTIME],
-      description: 'Python lambda layer for requests',
-      layerVersionName: requestsLambdaLayerId
-    });
+    this.requestsLambdaLayer = lambda.LayerVersion.fromLayerVersionArn(this, requestsLambdaLayerId, LambdaConfig.REQUESTS_LAMBDA_LAYER_ARN);
 
     const dateutilsLambdaLayerId = DefaultIdBuilder.build('dateutils-lambda-layer');
-    this.dateutilsLambdaLayer = new lambda.LayerVersion(this, dateutilsLambdaLayerId, {
-      code: lambda.Code.fromAsset(LambdaConfig.getLambdaPythonLayerPath('dateutils')),
-      compatibleRuntimes: [LambdaConfig.DEFAULT_PYTHON_RUNTIME],
-      description: 'Python lambda layer for dateutils',
-      layerVersionName: dateutilsLambdaLayerId
-    });
+    this.dateutilsLambdaLayer = lambda.LayerVersion.fromLayerVersionArn(this, dateutilsLambdaLayerId, LambdaConfig.DATEUTILS_LAMBDA_LAYER_ARN);
 
     const numpyLambdaLayerId = DefaultIdBuilder.build('numpy-lambda-layer');
-    this.numpyLambdaLayer = new lambda.LayerVersion(this, numpyLambdaLayerId, {
-      code: lambda.Code.fromAsset(LambdaConfig.getLambdaPythonLayerPath('numpy')),
-      compatibleRuntimes: [LambdaConfig.DEFAULT_PYTHON_RUNTIME],
-      description: 'Python lambda layer for numpy',
-      layerVersionName: numpyLambdaLayerId
-    });
+    this.numpyLambdaLayer = lambda.LayerVersion.fromLayerVersionArn(this, numpyLambdaLayerId, LambdaConfig.NUMPY_LAMBDA_LAYER_ARN);
 
     const pandasLambdaLayerId = DefaultIdBuilder.build('pandas-lambda-layer');
-    this.pandasLambdaLayer = new lambda.LayerVersion(this, pandasLambdaLayerId, {
-      code: lambda.Code.fromAsset(LambdaConfig.getLambdaPythonLayerPath('pandas')),
-      compatibleRuntimes: [LambdaConfig.DEFAULT_PYTHON_RUNTIME],
-      description: 'Python lambda layer for pandas',
-      layerVersionName: pandasLambdaLayerId
-    });
+    this.pandasLambdaLayer = lambda.LayerVersion.fromLayerVersionArn(this, pandasLambdaLayerId, LambdaConfig.PANDAS_LAMBDA_LAYER_ARN);
 
     const yfinanceLambdaLayerId = DefaultIdBuilder.build('yfinance-lambda-layer');
-    this.yfinanceLambdaLayer = new lambda.LayerVersion(this, yfinanceLambdaLayerId, {
-      code: lambda.Code.fromAsset(LambdaConfig.getLambdaPythonLayerPath('yfinance')),
-      compatibleRuntimes: [LambdaConfig.DEFAULT_PYTHON_RUNTIME],
-      description: 'Python lambda layer for yfinance',
-      layerVersionName: yfinanceLambdaLayerId
-    });
+    this.yfinanceLambdaLayer = lambda.LayerVersion.fromLayerVersionArn(this, yfinanceLambdaLayerId, LambdaConfig.YFINANCE_LAMBDA_LAYER_ARN);
   }
 }
