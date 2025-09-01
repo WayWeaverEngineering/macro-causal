14e086d91d608028970bd3560e8863a99fb9c58d
harry-nguyen-wayweaver <harrynguyen@wayweaver.com>
2025-09-01 14:56:16 -0400

    Updated api domain cert

--- backend/cdk/infrastructure/configs/AwsConfig.ts
+++ backend/cdk/infrastructure/configs/AwsConfig.ts
@@ -2,7 +2,7 @@ import { Duration } from "aws-cdk-lib"
 
 export class AwsConfig {
   static readonly WEB_DOMAIN_CERTIFICATE_ARN = "arn:aws:acm:us-east-1:715067592333:certificate/6ff63db0-df16-4fc5-8302-bd14b3da1664"
-  static readonly API_DOMAIN_CERTIFICATE_ARN = "arn:aws:acm:us-east-1:715067592333:certificate/62d54e12-aa93-4e84-a840-b33fc5833e0e"
+  static readonly API_DOMAIN_CERTIFICATE_ARN = "arn:aws:acm:us-east-1:715067592333:certificate/fbf202e6-1365-4265-827a-7510cd0f4ff7"
 
   static readonly OPENAI_API_SECRET_ARN = "arn:aws:secretsmanager:us-east-1:715067592333:secret:sec-ai-analyst-openai-api-secrets-sV98gJ"
   static readonly OPENAI_API_SECRET_ID = "sec-ai-analyst-openai-api-secrets"
